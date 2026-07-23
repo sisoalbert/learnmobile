96575b4600a95bdb6433673ca85b87ea02c3b759
Siso <sisoalbert@yahoo.com>
2026-07-23 12:17:43 +0200

    feat: increase splash screen image size and configure fade animation duration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,11 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   return <Stack />;
